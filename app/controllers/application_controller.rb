8c70eec74871c521f57ffaba2d30adcea1a58a77
Gerome Pangan <gerome0123@gmail.com>
2018-04-06 15:42:04 +0800

    application controller update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   #
-  before_action :authenticate_author!
+  before_action :authenticate_user!
+  
+  alias currrnt_author current_user
 
   rescue_from CanCan::AccessDenied, with: :unauthorized
 
